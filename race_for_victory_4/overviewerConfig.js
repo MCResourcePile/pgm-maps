9c55d66a3fd686d8dbba305f67c8da60591e4de3
CrazyisCreeps <MCResourcePile@gmail.com>
2022-02-05 08:15:18 +0000

    Deploy to GitHub Pages - 533bc9e319a2194e8ca84366950c349b8920be12

--- race_for_victory_4/overviewerConfig.js
+++ race_for_victory_4/overviewerConfig.js
@@ -27,7 +27,7 @@ var overviewerConfig = {
     ],
     "map": {
         "debug": false,
-        "cacheTag": "1621691909",
+        "cacheTag": "1644048882",
         "north_direction": "lower-left",
         "controls": {
             "pan": true,
@@ -49,7 +49,7 @@ var overviewerConfig = {
             "base": "",
             "bgcolor": "#1a1a1a",
             "world": "world",
-            "last_rendertime": 1588053599,
+            "last_rendertime": 1618951693,
             "imgextension": "png",
             "isOverlay": false,
             "poititle": "Markers",
