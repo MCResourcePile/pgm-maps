19b7b1bbeddc1d319a0358fd29ffba7884894546
mitchts <MCResourcePile@gmail.com>
2023-02-18 01:52:32 +0000

    Deploy to GitHub Pages - 70514827a4242b70bbbc060a85533ec042ddabe8

--- copypasta/overviewerConfig.js
+++ copypasta/overviewerConfig.js
@@ -27,7 +27,7 @@ var overviewerConfig = {
     ],
     "map": {
         "debug": false,
-        "cacheTag": "1676684465",
+        "cacheTag": "1659424569",
         "north_direction": "lower-left",
         "controls": {
             "pan": true,
@@ -49,7 +49,7 @@ var overviewerConfig = {
             "base": "",
             "bgcolor": "#1a1a1a",
             "world": "world",
-            "last_rendertime": 1676684188,
+            "last_rendertime": 1651817149,
             "imgextension": "png",
             "isOverlay": false,
             "poititle": "Markers",
